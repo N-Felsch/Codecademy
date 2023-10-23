6dca038af8b301a66e6c1e96f2e5967978561c77
Nicholas Felsch <143032403+N-Felsch@users.noreply.github.com>
2023-10-22 21:51:18 -0600

    Started on Mad Libs project.

---
+++ DataTypesandVariables/MadLibsProject.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace MadLibs
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      /*
+      This program is a mad lib program. It takes information from the user and creates a funny random story based on the information given.
+
+      Author: Nicholas F
+      */
+
+
+      // Let the user know that the program is starting:
+      Console.WriteLine("Hello! We are about to begin our Mad Lib program, please enjoy this fun experience, if you so desire.");
+
+
+      // Give the Mad Lib a title:
+      string title = "The Ocean that Could";
+
+      Console.WriteLine(title);
+      // Define user input and variables:
+
+
+      // The template for the story:
+
+      string story = "This morning _ woke up feeling _. 'It is going to be a _ day!' Outside, a bunch of _s were protesting to keep _ in stores. They began to _ to the rhythm of the _, which made all the _s very _. Concerned, _ texted _, who flew _ to _ and dropped _ in a puddle of frozen _. _ woke up in the year _, in a world where _s ruled the world.";
+
+
+      // Print the story:
+
+    }
+  }
+}
