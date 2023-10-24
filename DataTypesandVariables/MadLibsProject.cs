61c2116045021ea411f900e0d2996b926ac6f7b0
Nicholas Felsch <nicholasfelsch@outlook.com>
2023-10-23 22:02:38 -0600

    Continued to work on Mad Lib project.

--- DataTypesandVariables/MadLibsProject.cs
+++ DataTypesandVariables/MadLibsProject.cs
@@ -19,10 +19,58 @@ namespace MadLibs
 
       // Give the Mad Lib a title:
       string title = "The Ocean that Could";
-
       Console.WriteLine(title);
-      // Define user input and variables:
 
+      // Define user input and variables:
+      Console.WriteLine("Enter a name: ");
+      string name = Console.ReadLine();
+
+      // Asking questions to fill Mad Lib
+      Console.WriteLine("Enter an adjective please: ");
+
+      // Helping those who don't know what an
+      // adjective is, giving a description.
+      Console.WriteLine("\n(An adjective is a word that describes a noun, like a color (‘blue’), or feeling (‘silly’), texture  (‘soft’).)\n");
+      // Back to the program
+
+      string adjOne = Console.ReadLine();
+      Console.WriteLine("Enter another adjective please: ");
+      string adjTwo = Console.ReadLine();
+      Console.WriteLine("Last adjective (I promise) please: ");
+      string adjThree = Console.ReadLine();
+
+      Console.WriteLine("Please enter a verb: ");
+      
+      // Helping those who do not know what a verb is
+      Console.WriteLine("\n(A verb is a word that represents an action, like ‘sit’, ‘eat’, ‘sleep’)\n");
+      // Back to the program
+      string verb = Console.ReadLine();
+      
+      Console.WriteLine("Please enter a noun: ");
+
+      // Helping those who do not know what a noun is
+      Console.WriteLine("\n(A noun is a person (‘girl’), place (‘cabin’), or thing (‘toaster’).\n");
+      // Back to the program
+
+      string nounOne = Console.ReadLine();
+      Console.WriteLine("Please enter a second noun: ");
+      string nounTwo = Console.ReadLine();
+      
+      // Random questions
+      Console.WriteLine("Please enter an animal: ");
+      string animal = Console.ReadLine();
+      Console.WriteLine("Please enter a food: ");
+      string food = Console.ReadLine();
+      Console.WriteLine("Please enter a fruit: ");
+      string fruit = Console.ReadLine();
+      Console.WriteLine("Please enter a superhero: ");
+      string superhero = Console.ReadLine();
+      Console.WriteLine("Please enter a country: ");
+      string country = Console.ReadLine();
+      Console.WriteLine("Please enter a dessert: ");
+      string dessert = Console.ReadLine();
+      Console.WriteLine("Please enter a year: ");
+      string year = Console.ReadLine();
 
       // The template for the story:
 
