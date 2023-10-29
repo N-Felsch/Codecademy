e52f768d9ffc5d335a8538d670e70b1c9b5093ac
Nicholas Felsch <nicholasfelsch@outlook.com>
2023-10-28 20:44:18 -0600

    Finished money coin converter project.

--- MoneyMakerProject/MoneyMaker.cs
+++ MoneyMakerProject/MoneyMaker.cs
@@ -2,36 +2,41 @@ using System;
 
 namespace MoneyMaker
 {
-  class MainClass
-  {
-    public static void Main(string[] args)
+    class MainClass
     {
-      Console.WriteLine("Welcome to Money Maker!");
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Money Maker!");
 
-      // Ask user for amount to convert
-      Console.WriteLine("Please enter a value you'd like to convert to coins:");
-      string userInput = Console.ReadLine();
+            // Ask user for amount to convert
+            Console.WriteLine("Please enter a value you'd like to convert to coins:");
+            string userInput = Console.ReadLine();
       
-      // Convert to double
-      double userInputConv = Convert.ToDouble(userInput);
+            // Convert to double
+            double userInputConv = Convert.ToDouble(userInput);
 
-      // Repeat users input
-      Console.WriteLine($"{userInput} cents is equal to . . .");
+            // Repeat users input
+            Console.WriteLine($"{userInput} cents is equal to . . .");
 
-      // Setting variables to use for our equations
-      int goldCoin = 10;
-      int silverCoin = 5;
+            // Setting variables to use for our equations
+            int goldCoin = 10;
+            int silverCoin = 5;
       
-      // Equations
-      double goldCoins = Math.Floor(userInputConv / goldCoin);
-      double goldCoinsRemain = (userInputConv % goldCoin);
-      double silverCoins = Math.Floor(goldCoinsRemain / 5);
-      double silverCoinsRemain = (silverCoins % silverCoin);
+            // Equations
+            double goldCoins = Math.Floor(userInputConv / goldCoin);
+            double goldCoinsRemain = (userInputConv % goldCoin);
+            double silverCoins = Math.Floor(goldCoinsRemain / 5);
+            double silverCoinsRemain = ( goldCoinsRemain % silverCoin);
+
+            // Return amount of coins / answer
+            Console.WriteLine("Gold coins: " + goldCoins);
+            Console.WriteLine("Silver coins: " + silverCoins);
+            Console.WriteLine("Bronze coins: " + (silverCoinsRemain));
 
 
-      // Debug
-      Console.WriteLine(silverCoinsRemain);
+            // Debug
       
+      
+        }
     }
-  }
 }
