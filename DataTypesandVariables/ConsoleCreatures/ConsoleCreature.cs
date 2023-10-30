bd1a0fb4aebfc43b07383b37b65766e3d55d6d38
Nicholas Felsch <143032403+N-Felsch@users.noreply.github.com>
2023-10-29 21:33:19 -0600

    Console Creatures

---
+++ DataTypesandVariables/ConsoleCreatures/ConsoleCreature.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // Here in the program we are creating an ASCII Creature (artwork from text)
+      Console.WriteLine(" .-.");  
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| 0 |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("\'~~~'");
+      Console.WriteLine("Pebble");
+     }
+  }
+}
