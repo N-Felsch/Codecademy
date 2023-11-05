54581687ead635fe9c33bab027569e7aec66711f
Nicholas Felsch <nicholasfelsch@outlook.com>
2023-11-04 20:55:35 -0600

    Went back to debug my code and fix errors.

--- LogicandConditionals/PasswordChecker.cs
+++ LogicandConditionals/PasswordChecker.cs
@@ -2,56 +2,42 @@ using System;
 
 namespace PasswordChecker
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
 
-      // Defining password parameters
-      int minLength = 0;
-      string uppercase = userpass;
-      string lowercase;
-      string digits;
-      string specialChars;
+            // Defining password parameters
+            int minLength = 0;
+            string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string lowercase;
+            string digits;
+            string specialChars;
 
-      // Getting user input for password
-      Console.WriteLine("Enter a new password please.");
-      string userPass = Console.ReadLine();
+            // Getting user input for password
+            Console.WriteLine("Enter a new password please.");
+            string userPass = Console.ReadLine();
 
-      // Password score-r
-      int score = 0;
+            // Password score-r
+            int score = 0;
 
-      // Equation for scoring
-      if (userPass >= minLength) 
-      {
-        score++;
-      }
+            // Equation for scoring
+            if (userPass.Length >= minLength) 
+            {
+                score++;
+            }
 
-      if (Tools.Contains(userPass, uppercase))
-      {
-        score++;
-      }
+            if (Tools.Contains(userPass, uppercase))
+            {
+                score++;
+            }
 
-      if (Tools.Contains(userPass, lowercase))
-      {
-        score++;
-      }
 
-      if (Tools.Contains(userPass, digits))
-      {
-        score++;
-      }
-
-      if (Tools.Contains(userPass, specialsChars))
-      {
-        score++;
-      }
-
-      // Debug
-      Console.WriteLine(score);
+            // Debug
+            Console.WriteLine(score);
 
 
 
+        }
     }
-  }
 }
