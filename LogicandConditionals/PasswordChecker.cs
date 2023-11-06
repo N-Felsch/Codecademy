7fd759f80603a5e9dc7b81284af742b66196a741
Nicholas Felsch <nicholasfelsch@outlook.com>
2023-11-05 20:45:56 -0700

    Finishing up Password Checker, still some breaks, but working on testing

--- LogicandConditionals/PasswordChecker.cs
+++ LogicandConditionals/PasswordChecker.cs
@@ -8,11 +8,11 @@ namespace PasswordChecker
         {
 
             // Defining password parameters
-            int minLength = 0;
+            int minLength = 5;
             string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            string lowercase;
-            string digits;
-            string specialChars;
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+            string digits = "0123456789";
+            string specialChars = "!@#$%^&*()_+={}[]|\":>?<`~";
 
             // Getting user input for password
             Console.WriteLine("Enter a new password please.");
@@ -32,9 +32,47 @@ namespace PasswordChecker
                 score++;
             }
 
+            if (Tools.Contains(userPass, lowercase))
+            {
+                score++;
+            }
+
+            if (Tools.Contains(userPass, digits))
+            {
+                score++;
+            }
+
+            if (Tools.Contains(userPass, specialChars))
+            {
+                score++;
+            }
+
+            // Switch for telling user how they did
+            switch (score)
+            {
+                case 4:
+                case 5:
+                    Console.WriteLine("Extremely Strong.");
+                    break;
+                case 3:
+                    Console.WriteLine("Strong.");
+                    break;
+                case 2:
+                    Console.WriteLine("Medium.");
+                    break;
+                case 1:
+                    Console.WriteLine("Weak.");
+                    break;
+                default:
+                    Console.WriteLine("The password doesn't meet any standards.");
+                    break;
+            }
+
 
             // Debug
             Console.WriteLine(score);
+
+            // TEST PROGRAM, HAS BREAKS!!!!!
 
 
 
